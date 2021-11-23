15d649a3c51774300e2a0dfc277678f5f356d775
andrey <andreytorrente@gmail.com>
2021-11-23 17:12:27 -0600

    update files

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@
 window.onload = function(){
     document.querySelector(".preloader").style.display = "none";
 
+
 }
 
 
@@ -14,3 +15,59 @@ burger.addEventListener('click', () =>{
     menuContent.classList.toggle('menu__active');
 
 })
+
+
+
+const heart = document.querySelectorAll('.fa-heart');
+
+
+heart.forEach(heart =>{
+    heart.addEventListener('click',()=>{
+
+        heart.classList.toggle('heart-clicked');
+
+    } )
+})
+
+
+
+
+
+
+
+
+// tours swiper script
+var tourSwiper = new Swiper(".tours-swiper", {
+               
+    spaceBetween: 5,
+    freeMode: true,
+    loop:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        180:{
+            slidesPerView: 1,
+
+        },
+        300:{
+            slidesPerView: 2,
+
+        },
+        600:{
+            slidesPerView: 3,
+
+        },
+       
+        1200:{
+            slidesPerView: 4,
+        }
+
+    }
+});
