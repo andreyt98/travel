88a770ded95d068eb65f17eafc48fc1ac64ca93f
andrey <andreytorrente@gmail.com>
2022-01-28 02:43:56 -0600

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const uglify = require("gulp-uglify");
+const htmlmin = require("gulp-htmlmin");
+const imgmin = require("gulp-imagemin");
+
 
 function compileSass() {
   return src("./src/sass/**/*.scss")
@@ -20,6 +23,19 @@ function uglifyjs() {
   return src("./src/js/*.js").pipe(uglify()).pipe(dest("./public/js"));
 }
 
-exports.compileSass = compileSass;
+function minifyhtml() {
+  return src("./*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("./public/"));
+}
+
+function minimg() {
+  return src("./img/**/*").pipe(imgmin()).pipe(dest("./public/img"));
+}
+
+exports.compileSass = compileSass; // not necessary to run, 'watchSass' function will run this one
+
 exports.watchSass = watchSass;
 exports.uglifyjs = uglifyjs;
+exports.minifyhtml = minifyhtml;
+exports.minimg = minimg;
