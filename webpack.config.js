0475da680578c2b1f38ed6af8e3f1eaab381a4ae
andrey <andreytorrente@gmail.com>
2022-04-27 09:16:00 -0600

    added SASS loaders

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "production",
+
+  devServer: {
+    watchFiles: ["./src/*"],
+    open: true,
+    hot: true,
+  },
+
+  output: {
+    filename: "main.js",
+    clean: true,
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
+        exclude: /style\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /style\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          sources: false,
+          minimize: true,
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif|webp)%/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/assets",
+          to: "assets/",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
+
