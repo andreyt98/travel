4567132a36a11de186ad3fbce606eafffacda697
andrey <andreytorrente@gmail.com>
2022-05-26 16:24:45 -0600

    change footer structure and form style

--- src/js/submitEmail.js
+++ src/js/submitEmail.js
@@ -1,16 +1,17 @@
 export const submitEmail = () => {
    
-    const email = document.querySelector(".footer input[type=email]");
+    const form = document.querySelector(".footer form" );
+    const email = document.querySelector(".footer form input[type=email]");
     const banner = document.querySelector(".subscribe-banner");
     
     // regular expression to validate email
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     
     if (email.value == "") {
-        email.style.border = "1px solid red";
+        form.style.border = "1px solid red";
         
         setTimeout(function () {
-            email.style.border = "none";
+            form.style.border = "none";
         }, 2000);
     }
     
@@ -19,5 +20,4 @@ export const submitEmail = () => {
         email.value = "";
         banner.style.left = "0%";
     }
-    
 }
