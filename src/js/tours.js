9bfca17e7c3eaf3ba59cbd23524ce6cabcccfae9
andrey <andreytorrente@gmail.com>
2022-05-27 18:27:54 -0600

    delete unused code

--- src/js/tours.js
+++ src/js/tours.js
@@ -1,54 +1,53 @@
 export const createTourElements = () => {
   // creation of hotel section content
- 
-  const tourNames= [
-     "Snorkelling in the Maldives",
-     "Exploring Peru",
-     "Cruise the Antartic Peninsula",
-     "Wildlife of Africa",
-     "Treasures of Beijing",
-     "Central America Discovery",
-     "Float down the treasures of Egip",
-     "Camp under Findland lights",
-     "Landscape tent in Mongolia",
-     "Historical tour in Georgia",
+
+  const tourNames = [
+    "Snorkelling in the Maldives",
+    "Exploring Peru",
+    "Cruise the Antartic Peninsula",
+    "Wildlife of Africa",
+    "Treasures of Beijing",
+    "Central America Discovery",
+    "Float down the treasures of Egip",
+    "Camp under Findland lights",
+    "Landscape tent in Mongolia",
+    "Historical tour in Georgia",
   ];
-  // const slider = document.querySelector(".slider");
-  // const DocumentFrag = document.createDocumentFragment();
+
   const $sliderContainer = document.createElement("DIV");
   $sliderContainer.classList.add("slider-container");
-$sliderContainer.innerHTML = `
+  $sliderContainer.innerHTML = `
 <div class="slider-header">
   <h2> | View our weekly tours!</h2>
   <div class="controls">
-   <p class="left"> < </p>
-   <p class="right"> > </p>
+   <p class="left"> &#x276E; </p>
+   <p class="right"> &#x276F;  </p>
   </div>
   `;
 
   const $slider = document.createElement("DIV");
-      $slider.classList.add("slider");
+  $slider.classList.add("slider");
 
-      for (let i = 1; i <= 10; i++) {
-      const $content = document.createElement("DIV");
-        $content.classList.add("content");
-        $content.innerHTML = `
+  for (let i = 1; i <= 10; i++) {
+    const $content = document.createElement("DIV");
+    $content.classList.add("content");
+    $content.innerHTML = `
         <img src= ./assets/img/tours/tour-${i}.webp class="content-img"></img>
           <div class="overlay-bg">
             <p>${tourNames[i]}</p>
             <p>$2000</p>
           </div> 
         `;
-        $slider.append($content);
+    $slider.append($content);
   }
-  $sliderContainer.append($slider)
-  document.querySelector('#section-two').append($sliderContainer);
+  $sliderContainer.append($slider);
+  document.querySelector("#section-two").append($sliderContainer);
 
-  document.addEventListener('click', (evt) =>{
+  document.addEventListener("click", (evt) => {
     const elementToSlide = evt.target.parentElement.parentElement.nextElementSibling;
     const screenWidth = screen.width;
 
     if (evt.target.matches(`.left`)) elementToSlide.scrollBy(-(screenWidth / 2), 0);
     if (evt.target.matches(`.right`)) elementToSlide.scrollBy(screenWidth / 2, 0);
-  })
+  });
 };
