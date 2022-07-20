0c38c72d03c1b7ff6b3476ab331fe3f9d098de69
andrey <andreytorrente@gmail.com>
2022-07-20 01:41:51 -0600

    add hotels card component and helper function containing an object with their names and location

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+export const Card = (element,name) => {
+    const card = document.createElement('DIV')
+    card.classList.add('hotel-card-grid')
+    card.innerHTML=
+`   
+        <img src="../assets/img/hotels/hotel-${element}.webp" alt="hotel${element}" width="150" height="95" loading="lazy">
+        
+        <div class="hotel-info">
+            <p>${name}</p>
+            <p>$2000</p>
+            <p class="stars">★ ★ ★ ★ ★</p>
+        </div>
+`
+
+    return card;
+}
