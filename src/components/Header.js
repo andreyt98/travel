cdcf10472646ef9abe27cf99ff0e996593701423
andrey <andreytorrente@gmail.com>
2022-08-08 19:59:09 -0600

    fixed wrong 'scroll to' calculations

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,13 +24,11 @@ window.addEventListener("scroll", () => {
 
 document.addEventListener("click", (e) => {
 
-
-
   // scrollTo events
   if (e.target.matches(".nav-links").firstChild || e.target.matches(".logo") ) {
     e.preventDefault();
     
-    const sectionOne = document.getElementById("root").offsetTop - document.querySelector(".nav").offsetHeight - 40;
+    const sectionOne = document.querySelector(".nav");
     
     window.scrollTo(0, sectionOne);
   }
@@ -38,19 +36,22 @@ document.addEventListener("click", (e) => {
   if (e.target.matches(".hotels-link")) {
     e.preventDefault();
     
-    const sectionOne = document.getElementById("section-one").offsetTop - document.querySelector(".nav").offsetHeight - 40;
+    const sectionOne = window.innerWidth < 640 ? document.getElementById("section-one").offsetTop - 58 : 
+    document.getElementById("section-one").offsetTop - document.querySelector(".nav").offsetHeight - 40;
     
     window.scrollTo(0, sectionOne);
   }
   if (e.target.matches(".tours-link")) {
     e.preventDefault();
-    const sectionTwo = document.getElementById("section-two").offsetTop - document.querySelector(".nav").offsetHeight - 20;
+    const sectionTwo = window.innerWidth < 640 ?document.getElementById("section-two").offsetTop - 35 : 
+    document.getElementById("section-two").offsetTop - document.querySelector(".nav").offsetHeight - 20;
     
     window.scrollTo(0, sectionTwo);
   }
   if (e.target.matches(".more-link")) {
     e.preventDefault();
-    const sectionThree = document.getElementById("section-three").offsetTop - document.querySelector(".nav").offsetHeight;
+    const sectionThree = window.innerWidth < 640 ? document.getElementById("section-three").offsetTop - 35 :
+    document.getElementById("section-three").offsetTop - document.querySelector(".nav").offsetHeight;
     window.scrollTo(0, sectionThree);
   }
 
--- src/helpers/scrollTo.js
+++
@@ -1,9 +0,0 @@
-// sections
-export const hotelSection = document.getElementById("section-one");
-export const tourSection = document.getElementById("section-two");
-export const footerSection = document.getElementById("section-three");
-
-export const scrollTo = (e, section) => {
-  e.preventDefault();
-  section.scrollIntoView({ behavior: "smooth" });
-}
